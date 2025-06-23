f69481d0a3a10d2fde99188e684b68cea606aab5
Emiliano Rios <emirioslp@gmail.com>
2025-06-23 02:02:49 -0300

    jijo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,22 +9,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-// Ruta simple para probar si el backend responde
 app.get('/api', (_, res) => {
   res.send('Backend activo (independientemente de la DB)');
 });
-
-// Intentamos conectar a la base de datos
-AppDataSource.initialize()
-  .then(() => {
-    console.log('Conexión a la base de datos exitosa');
-  })
-  .catch((err) => {
-    console.error('Falló la conexión a la base de datos');
-    console.error(err.message);
-  })
-  .finally(() => {
-    app.listen(PORT, () => {
-      console.log(`Servidor corriendo en http://localhost:${PORT}`);
-    });
-  });
